1396d8480d31f0e2bef02a9fefe933c627ea9012
Christian Stigen Larsen <csl@sublevel3.org>
2014-01-14 23:49:55 +0100

    Adds benchmarking for Python

---
+++ python/bench.py
@@ -0,0 +1,22 @@
+"""Measures parsing speed of dna traits."""
+
+import sys
+import timeit
+
+count = 70
+print("Measuring parsing speed (%d times) " % count)
+
+sys.stdout.write("[%s]\r[" % ("."*count))
+sys.stdout.flush()
+
+best = min(timeit.Timer(
+  """
+  dt.parse('../genome.txt')
+  sys.stdout.write("#");
+  sys.stdout.flush()
+  """,
+  setup = "import dna_traits as dt; import sys")
+  .repeat(count, 1))
+
+print("\nThe best speed was {}\n".format(best))
+print("We report the best time only to mitigate OS preemption noise.")
