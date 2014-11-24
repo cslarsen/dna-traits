fe6e32c676718864f1f4f8bb650cee9b2d21cb5e
Christian Stigen Larsen <csl@sublevel3.org>
2014-11-24 18:08:56 +0100

    Adds year and ethnicity parameters to parse

--- py-dnatraits/dna_traits/parse.py
+++ py-dnatraits/dna_traits/parse.py
@@ -8,6 +8,13 @@ Distributed under the GPL v3 or later. See COPYING.
 import _dna_traits
 from genome import Genome
 
-def parse(filename, orientation=+1):
-    """Parses 23andMe text file and returns a Genome."""
-    return Genome(_dna_traits.parse(filename), orientation)
+def parse(filename, orientation=+1, year=None, ethnicity=None):
+    """Parses 23andMe text file and returns a Genome.
+
+    Arguments:
+        orientation: Whether genotype is minus (-1) or plus (+1).
+        year: Year of birth for individual (optional).
+        ethnicity: Ethnicity for individial (optional).
+    """
+    return Genome(_dna_traits.parse(filename), orientation, year=year,
+            ethnicity=ethnicity.lower())
