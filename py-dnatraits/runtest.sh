9d5b52da16d689b1cd509c9bdbb0c79577e053df
Christian Stigen Larsen <csl@csl.name>
2016-09-06 00:32:48 +0200

    Workaround for OSX

--- py-dnatraits/runtest.sh
+++ py-dnatraits/runtest.sh
@@ -3,4 +3,8 @@ set -e
 PYTHON=$1
 SOURCEDIR=$2
 BINDIR=$3
-PYTHONPATH=${BINDIR}/py-dnatraits:${SOURCEDIR} ${PYTHON} ${SOURCEDIR}/test/test_dna_traits.py
+
+# Hack for OSX: Ignore PYTHONEXECUTABLE, assume it was compiledusing the system
+# Python
+#PYTHONPATH=${BINDIR}/py-dnatraits:${SOURCEDIR} ${PYTHON} ${SOURCEDIR}/test/test_dna_traits.py
+PYTHONPATH=${BINDIR}/py-dnatraits:${SOURCEDIR} /usr/bin/python ${SOURCEDIR}/test/test_dna_traits.py
