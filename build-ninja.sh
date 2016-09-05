669fa4e6136760cbb3bdbe3d14985be0711bc256
Christian Stigen Larsen <csl@csl.name>
2016-09-05 19:40:34 +0200

    Specifies build type

--- build-ninja.sh
+++ build-ninja.sh
@@ -2,4 +2,8 @@
 set -e
 rm -rf build
 mkdir build
-cd build; time (cmake .. -DCMAKE_BUILD_TYPE=Release -G 'Ninja' && ninja)
+cd build; time ( \
+  cmake .. \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_OSX_ARCHITECTURES=x86_64 \
+    -G 'Ninja' && ninja)
